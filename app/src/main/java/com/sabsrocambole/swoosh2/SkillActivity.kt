0fe195b87f8873e39f1a14979e12823f67051010
sabrinadevasconcelosfernandes <sabrinadvf@ibm.com>
2022-02-23 13:08:22 -0300

    commit 5 - seccond page finish starting the third page

--- app/src/main/java/com/sabsrocambole/swoosh2/SkillActivity.kt
+++ app/src/main/java/com/sabsrocambole/swoosh2/SkillActivity.kt
@@ -3,9 +3,9 @@ package com.sabsrocambole.swoosh2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class activity_league : BaseActivity() {
+class SkillActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_league)
+        setContentView(R.layout.activity_skill)
     }
 }
