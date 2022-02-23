84282846f3936c14daedfe86d20dd05303f8ef87
sabrinadevasconcelosfernandes <sabrinadvf@ibm.com>
2022-02-23 11:49:37 -0300

    fourth commit - get started button create

--- app/src/main/java/com/sabsrocambole/swoosh2/MainActivity.kt
+++ app/src/main/java/com/sabsrocambole/swoosh2/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.sabsrocambole.swoosh2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.service.controls.templates.ControlButton
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val getStartedButton = findViewById<Button>(R.id.getStartedBtn)
+        getStartedButton.setOnClickListener {
+            val leagueIntent = Intent(this, activity_league::class.java )
+            startActivity(leagueIntent)
+        }
+
+
     }
 }
